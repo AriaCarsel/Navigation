87ea8c0bbf9ed3fceba021025ebee99750703615
3311460487 <email@3311460487.com>
2019-10-28 21:44:14 +0800

    Initial commit

---
+++ app/src/main/java/com/carsel/navigation/MyFragment.java
@@ -0,0 +1,22 @@
+package com.carsel.navigation;
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+@SuppressLint("ValidFragment")
+public class MyFragment extends Fragment {
+
+    public MyFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg_content,container,false);
+
+        return view;
+    }
+}
